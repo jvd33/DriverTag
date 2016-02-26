121e17f66cfa731878b64ad884b5bda881c5281c
Nathan Perry <nbp8557@rit.edu>
2016-02-26 13:51:41 -0500

    Added the models to the initilization

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,41 @@
 __author__ = 'SWEN356 Team 4'
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '\xa5\x8f\x19\xbb`$\xacw\x91\xe1\xd2\x896R\xf9\x14\x01\xe1\xd5U\xcc\xa9\x13'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:postgresql@localhost/DriverTag'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     timestamp = db.Column(db.DateTime)
+     x_gyroscope = db.Column(db.NUMERIC)
+     y_gyroscope = db.Column(db.NUMERIC)
+     z_gyroscope = db.Column(db.NUMERIC)
+     x_accelorometer = db.Column(db.NUMERIC)
+     y_accelorometer = db.Column(db.NUMERIC)
+     z_accelorometer = db.Column(db.NUMERIC)
+     latitude = db.Column(db.NUMERIC)
+     longitude = db.Column(db.NUMERIC)
+
+     ''' Foreign Key for a User '''
+     user_id = db.Column(db.INTEGER, db.ForeignKey('user.id'))
+     user = db.relation('User', backref=db.backref('data',lazy='dynamic'))
+
+     def __int__(self, xgyro,ygyro,zgyro,xaccel,yaccel,zaccel,latitude,longitude):
+         self.x_gyroscope = xgyro
+         self.y_gyroscope = ygyro
+         self.z_gyroscope = zgyro
+         self.x_accelorometer = xaccel
+         self.y_accelorometer = yaccel
+         self.z_accelorometer = zaccel
+         self.latitude = latitude
+         self.longitude = longitude
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
 
 from app import views
